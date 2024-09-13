09ad99d9879d3a3da30e0a696cb730c7fc94265d
Michael Nwachukwu <dahformulah@gmail.com>
2024-09-13 17:08:24 +0100

    update

--- contracts/Event.sol
+++ contracts/Event.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.24;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
 contract EventContract {
 
